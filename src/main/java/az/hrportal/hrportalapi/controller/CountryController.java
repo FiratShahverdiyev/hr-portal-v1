fe35c6a591b52d2f5cc234934b2ea6a7283c1cd3
Firat Shahverdiyev <firat.sahverdiyev.45@gmail.com>
2021-08-21 00:07:05 +0400

    feature(Authentication) header for CountryController

--- src/main/java/az/hrportal/hrportalapi/controller/CountryController.java
+++ src/main/java/az/hrportal/hrportalapi/controller/CountryController.java
@@ -3,6 +3,7 @@ package az.hrportal.hrportalapi.controller;
 import az.hrportal.hrportalapi.dto.KeyValue;
 import az.hrportal.hrportalapi.dto.ResponseDto;
 import az.hrportal.hrportalapi.service.CountryService;
+import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class CountryController {
     private final CountryService countryService;
 
     @GetMapping
+    @ApiImplicitParam(name = "Authorization", required = true, paramType = "header", dataType = "String")
     public ResponseDto<List<KeyValue<String, Integer>>> getAll() {
         return ResponseDto.of(countryService.getAllCountries(), 200);
     }
