247f27b626cf76904fd90c966b7c8b6efd5f3db8
Firat Shahverdiyev <firat.sahverdiyev.45@gmail.com>
2021-08-21 15:53:28 +0400

    Ready for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar .\build\libs\hrportal-api-0.0.1-SNAPSHOT.war
+web: java -jar ./build/libs/hrportal-api-0.0.1-SNAPSHOT.war -Dserver.port=$PORT
--- src/main/java/az/hrportal/hrportalapi/service/AuthenticationService.java
+++ src/main/java/az/hrportal/hrportalapi/service/AuthenticationService.java
@@ -1,6 +1,8 @@
 package az.hrportal.hrportalapi.service;
 
+import az.hrportal.hrportalapi.domain.User;
 import az.hrportal.hrportalapi.dto.LoginRequestDto;
+import az.hrportal.hrportalapi.repository.UserRepository;
 import az.hrportal.hrportalapi.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
     private final TokenProvider tokenProvider;
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
     public String login(LoginRequestDto loginRequestDto) {
         log.info("login service started with username : {}", loginRequestDto.getUsername());
@@ -25,5 +31,17 @@ public class AuthenticationService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         log.info("login service completed with username : {}", loginRequestDto.getUsername());
         return tokenProvider.createToken(authentication);
+    }
+
+    @PostConstruct
+    @Transactional
+    protected void initUser() {
+        if (userRepository.count() > 0) {
+            return;
+        }
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("$2a$10$NrrK28KlvCBz47/FvKhksO6lEA4Io.yz2t/ChfbgbmAqQHemPhfuW");
+        userRepository.save(user);
     }
 }
