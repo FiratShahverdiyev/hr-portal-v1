d7d001a66f18096b1fbe2576a9c56135dd8f369f
Firat Shahverdiyev <firat.sahverdiyev.45@gmail.com>
2021-08-30 13:06:06 +0400

    feature(POST) post method for dropdown values

--- src/main/java/az/hrportal/hrportalapi/controller/VacancyController.java
+++ src/main/java/az/hrportal/hrportalapi/controller/VacancyController.java
@@ -2,10 +2,13 @@ package az.hrportal.hrportalapi.controller;
 
 import az.hrportal.hrportalapi.dto.DropDownResponseDto;
 import az.hrportal.hrportalapi.dto.ResponseDto;
+import az.hrportal.hrportalapi.dto.position.request.VacancyRequestDto;
 import az.hrportal.hrportalapi.service.position.VacancyService;
 import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class VacancyController {
     private final VacancyService vacancyService;
+
+    @PostMapping
+    @ApiImplicitParam(name = "Authorization", required = true, paramType = "header", dataType = "String")
+    public ResponseDto<Boolean> create(@RequestBody VacancyRequestDto vacancyRequestDto) {
+        return ResponseDto.of(vacancyService.create(vacancyRequestDto), 200);
+    }
 
     @GetMapping
     @ApiImplicitParam(name = "Authorization", required = true, paramType = "header", dataType = "String")
--- src/main/java/az/hrportal/hrportalapi/service/position/VacancyService.java
+++ src/main/java/az/hrportal/hrportalapi/service/position/VacancyService.java
@@ -1,6 +1,8 @@
 package az.hrportal.hrportalapi.service.position;
 
+import az.hrportal.hrportalapi.domain.position.Vacancy;
 import az.hrportal.hrportalapi.dto.DropDownResponseDto;
+import az.hrportal.hrportalapi.dto.position.request.VacancyRequestDto;
 import az.hrportal.hrportalapi.mapper.DropDownMapper;
 import az.hrportal.hrportalapi.repository.position.VacancyRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,15 @@ import java.util.List;
 public class VacancyService {
     private final VacancyRepository vacancyRepository;
     private final DropDownMapper dropDownMapper;
+
+    public Boolean create(VacancyRequestDto vacancyRequestDto) {
+        log.info("create service started with {}", vacancyRequestDto);
+        Vacancy vacancy = new Vacancy();
+        vacancy.setName(vacancyRequestDto.getName());
+        vacancyRepository.save(vacancy);
+        log.info("********** create service completed with {} **********", vacancyRequestDto);
+        return true;
+    }
 
     public List<DropDownResponseDto<String>> getAll() {
         log.info("getAllVacancies service started");
