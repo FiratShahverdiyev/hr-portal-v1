ddd44e681144118a0c5b4de8ce3a891f4fefc0c4
FiratShahverdiyev <firat.sahverdiyev.45@gmail.com>
2021-10-09 17:13:30 +0400

    refactor(KeepAlive)

--- src/main/java/az/hrportal/hrportalapi/controller/KeepAliveController.java
+++ src/main/java/az/hrportal/hrportalapi/controller/KeepAliveController.java
@@ -2,9 +2,11 @@ package az.hrportal.hrportalapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("keep-alive")
+@RestController
+@RequestMapping("keep-alive")
 @Slf4j
 public class KeepAliveController {
     @GetMapping
